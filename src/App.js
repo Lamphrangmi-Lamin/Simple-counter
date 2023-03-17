e3972e0d7fbc4cd37c61a752965f8d9e19695fab
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2023-03-17 11:09:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(`Changed to ${count}`);
+  });
+
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1);
+  const handleResetCount = () => setCount(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={handleIncrement}>Increment ⬆️</button>
+      <button onClick={handleDecrement}>Decrement ⬇️</button>
+      <button onClick={handleResetCount}>Reset Count</button>
+      <h1 id="count">{count}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Counter;
